7576706bee472c85c5e69b1466c22fe445aef932
1382515022@qq.com <1382515022@qq.com>
2022-09-14 15:37:39 +0800

    提案日期用新建日期

--- pm-api-service/src/main/java/com/donlim/pm/dto/IppProjectDto.java
+++ pm-api-service/src/main/java/com/donlim/pm/dto/IppProjectDto.java
@@ -43,6 +43,7 @@ public class IppProjectDto {
         /**
          * createDate
          */
+        @JsonProperty("CeateDate")
         private String createDate;
         /**
          * createMan
