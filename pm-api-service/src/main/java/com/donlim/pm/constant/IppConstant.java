52c4da87bd1f35fe9e673993f68cbcc29532153c
1382515022@qq.com <1382515022@qq.com>
2022-10-14 10:45:52 +0800

    修复BUG

--- pm-api-service/src/main/java/com/donlim/pm/constant/IppConstant.java
+++ pm-api-service/src/main/java/com/donlim/pm/constant/IppConstant.java
@@ -9,6 +9,6 @@ public class IppConstant {
     public static final String UI="UI设计";
     public static final String WEB_REVIEW="前端开发";
     public static final String CODE_REVIEW="后端开发";
-    public static final String REQUIRE_REVIEW="需求评审";
+    public static final String REQUIRE_REVIEW="规划";
     public static final String CODE_BACKUP="代码备份";
 }
