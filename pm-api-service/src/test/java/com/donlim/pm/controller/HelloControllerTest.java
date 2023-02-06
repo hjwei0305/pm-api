c9a9f7724c6745794c8af44e734a2f49652aaaa3
zhangxiaotong1@donlim.com <zhangxiaotong1@donlim.com>
2023-02-06 10:14:00 +0800

    init

--- pm-api-service/src/test/java/com/donlim/pm/controller/HelloControllerTest.java
+++ pm-api-service/src/test/java/com/donlim/pm/controller/HelloControllerTest.java
@@ -1,31 +1,22 @@
 package com.donlim.pm.controller;
 
-import com.alibaba.fastjson.JSONObject;
 import com.changhong.sei.basic.api.UserApi;
-import com.changhong.sei.basic.dto.FeatureRoleDto;
 import com.changhong.sei.core.context.ContextUtil;
-import com.changhong.sei.core.dto.ResultData;
-import com.changhong.sei.core.dto.serach.PageResult;
-import com.changhong.sei.core.dto.serach.Search;
 import com.changhong.sei.core.test.BaseUnitTest;
+import com.changhong.sei.core.util.JwtTokenUtil;
 import com.donlim.pm.connector.EipConnector;
 import com.donlim.pm.connector.IppConnector;
-import com.donlim.pm.dto.IppProjectInfoDetails;
 import com.donlim.pm.dto.MailDto;
-import com.donlim.pm.dto.PmBaseinfoDto;
-import com.donlim.pm.em.LogType;
-import com.donlim.pm.em.SmallNodeType;
 import com.donlim.pm.entity.PmBaseinfo;
 import com.donlim.pm.service.PmBaseinfoService;
 import com.donlim.pm.service.TodoListService;
-import com.donlim.pm.util.EnumUtils;
-import com.donlim.pm.webservice.eip.*;
 import org.junit.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 
-import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
+import java.util.UUID;
 
 /**
  * 实现功能: Hello 单元测试
@@ -39,13 +30,19 @@ public class HelloControllerTest extends BaseUnitTest {
     private  PmBaseinfoController pmBaseinfoController;
     @Autowired
     private UserApi userApi;
+    @Autowired
+    private TodoListService service;
 
-@Autowired
-private TodoListService todoListService;
     @Test
     public void sayHello() {
-        todoListService.SendEipTask();
 
+        MailDto mailDto=new MailDto();
+        mailDto.setAccount("377614");
+        mailDto.setUrl("https://sei.donlim.com/#/user/login");
+        mailDto.setMailID("1234");
+        mailDto.setMailType("ADD");
+        mailDto.setMailSubject("你有一个待办未处理，请登陆项目管理系统尽快处理！");
+        EipConnector.sendNotice(mailDto);
 
       /*  Search search=new Search();
         ResultData<PageResult<PmBaseinfoDto>> byPage = pmBaseinfoController.findByPage(search);
@@ -81,5 +78,53 @@ private TodoListService todoListService;
             }
         }*/
      // IppConnector.getTestResult("E20211008003");
+       @Test
+       public void a(){
+           Map<String, Object> claims = new HashMap<>();
+           String tenant = "test";
+           String userId = "用户id";
+           String account = "用户账号";
+           String userName = "用户姓名";
+           claims.put("tenant", tenant);
+           claims.put("userId", "B0FB4370-0BBB-11ED-BD40-0242AC140011");
+           claims.put("account", "376951");
+           claims.put("userName", "张晓橦");
+           claims.put("authorityPolicy","NormalUser");
 
+           JwtTokenUtil jwtTokenUtil = new JwtTokenUtil();
+           jwtTokenUtil.setJwtExpiration(2880000);
+           String t = jwtTokenUtil.generateToken(account, UUID.randomUUID().toString(), claims);
+           // 将token放入header中  x-authorization
+           System.out.println("Token: " + t);
+           account = jwtTokenUtil.getSubjectFromToken(t);
+           System.out.println(account);
+       }
+
+       @Test
+        public void b(){
+           service.SendEipTask();
+       }
+
+       @Test
+        public void c(){
+           IppConnector.getPorjectInfo("E20220415005");
+       }
+
+        @Test
+        public void d(){
+            List<PmBaseinfo> all = pmBaseinfoService.findAll();
+            for (PmBaseinfo pmBaseinfo : all) {
+                List<PmBaseinfo> porjectInfo = IppConnector.getPorjectInfo(pmBaseinfo.getCode());
+                if(porjectInfo.size()>0){
+                    pmBaseinfo.setSysName(porjectInfo.get(0).getSysName());
+                }
+            }
+            pmBaseinfoService.save(all);
+        }
+        @Test
+        public void w(){
+            String userName = ContextUtil.getUserName();
+            System.out.println(userName);
+
+        }
 }
--- pm-api-service/src/test/java/com/donlim/pm/controller/PmEmployeeControllerTest.java
+++ pm-api-service/src/test/java/com/donlim/pm/controller/PmEmployeeControllerTest.java
@@ -3,10 +3,13 @@ package com.donlim.pm.controller;
 import com.changhong.sei.core.dto.ResultData;
 import com.changhong.sei.core.test.BaseUnitTest;
 import com.changhong.sei.core.util.JsonUtils;
+import com.donlim.pm.dto.PmEmployeeDto;
+import com.donlim.pm.service.PmEmployeeService;
+import com.donlim.pm.service.PmOrganizeService;
+import com.donlim.pm.service.TodoListService;
 import org.junit.Assert;
 import org.junit.Test;
 import org.springframework.beans.factory.annotation.Autowired;
-import com.donlim.pm.dto.PmEmployeeDto;
 
 
 /**
@@ -19,6 +22,12 @@ public class PmEmployeeControllerTest extends BaseUnitTest {
 
     @Autowired
     private PmEmployeeController controller;
+    @Autowired
+    private PmEmployeeService pmEmployeeService;
+    @Autowired
+    private PmOrganizeService pmOrganizeService;
+    @Autowired
+    private TodoListService todoListService;
 
     @Test
     public void findOne() {
@@ -26,6 +35,26 @@ public class PmEmployeeControllerTest extends BaseUnitTest {
         ResultData<PmEmployeeDto> resultData = controller.findOne(id);
         LOG.debug(JsonUtils.toJson(resultData));
         Assert.assertTrue(resultData.successful());
+    }
+
+    @Test
+    public void test() {
+        // 同步时org要有tentcode
+        pmEmployeeService.synEmp();
+        System.out.println(1);
+    }
+
+    @Test
+    public void test1() {
+        // 同步时org要有tentcode
+        pmOrganizeService.synOrg();
+        System.out.println(1);
+    }
+
+    @Test
+    public void test2() {
+        todoListService.calcOverdueDay();
+
     }
 
 }
